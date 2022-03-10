babad5b0fa207f4060cc858cac67d1ac81d762ec
Levijs <39581290+levijskal@users.noreply.github.com>
2022-03-10 14:30:31 -0500

    Rewrite in TypeScript

--- index.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="@types/react" />
-
-declare function Visible(
-  when?: any,
-  children?: React.ReactNode,
-  fallback?: React.ReactElement<any>
-): React.ReactNode;
-
-export = Visible;
---
+++ tsup.config.ts
@@ -0,0 +1,71 @@
+import { defineConfig } from 'tsup'
+const baseConfig: object = {
+  /**
+      * Output different formats to different folder instead of using different extensions
+      */
+  legacyOutput: true,
+  format: [
+    `cjs`,
+    `esm`,
+  ],
+  minify: false,
+  outDir: `dist`,
+  sourcemap: false,
+  /** Don't bundle these modules */
+  external: [
+    `react`,
+    `react-dom`
+  ],
+  loader: {
+    '.md': `file`,
+    '.css': `css`,
+  },
+  /**
+      * Code splitting
+      * Default to `true`
+      * You may want to disable code splitting sometimes: #255
+      */
+  splitting: true,
+  /**
+      * Clean output directory before each build
+      */
+  clean: true,
+  /**
+      * Suppress non-error logs (excluding "onSuccess" process output)
+      */
+  silent: false,
+  /**
+      * Skip node_modules bundling
+      */
+  skipNodeModulesBundle: false,
+  /**
+      * Disable bundling, default to true
+      */
+  bundle: true,
+  /**
+      * Target platform
+      * @default `node`
+      */
+  platform: `browser`,
+  /**
+      * Use a custom tsconfig
+      */
+  tsconfig: `./tsconfig.json`,
+  target: `es2015`,
+}
+export default defineConfig([{
+  ...baseConfig,
+  name: `build`,
+  entry: [
+    `src/**/*`,
+    `!src/**/*.snap`,
+    `!**/*.md`,
+    `!src/components/**/*.test.{ts,tsx}`,
+    `!src/components/**/*.spec.{ts,tsx}`,
+    `test/**/*`,
+  ],
+  dts: {
+    entry: [`src/index.tsx`],
+    resolve: true,
+  },
+}])
